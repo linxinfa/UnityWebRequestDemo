37090ff0db01777bd1c69fc19a17241406ecc4dc
linxinfa <37791251+linxinfa@users.noreply.github.com>
2020-12-15 18:26:19 +0800

    UnityWebRequest Demo

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Net;
+
+public class Main : MonoBehaviour
+{
+    public Slider progressSlider;
+    public Text progressLbl;
+
+    public Button uploadBtn;
+    public Button downloadBtn;
+    public Button stopDownloadBtn;
+
+    private const string UPLOAD_URL = "http://localhost:8988";
+    private const string UPLOAD_FILE_NAME = "upload_test.png";
+    private const string DOWNLOAD_FILE_NAME = "download_test.png";
+
+
+    private IEnumerator m_downloadCoroutine = null;
+    private Stream m_serverResponseStream = null;
+    private Stream m_localFileStream = null;
+    /// <summary>
+    /// 下载读文件缓存，一帧读64个字节，这样可以很慢地写入本地文件，方便测试断点续传
+    /// </summary>
+    private byte[] m_downloadBuffer = new byte[128];
+
+    void Start()
+    {
+        // 上传文件
+        uploadBtn.onClick.AddListener(() =>
+        {
+            var filePath = Path.Combine(Application.dataPath, UPLOAD_FILE_NAME);
+            var fileName = Path.GetFileName(filePath);
+            byte[] data = ReadLogFile(filePath);
+            Debug.Log(data.Length);
+            StartCoroutine(UploadFile(UPLOAD_URL, fileName, data));
+        });
+
+        // 下载文件
+        downloadBtn.onClick.AddListener(() =>
+        {
+            StopDownload();
+            m_downloadCoroutine = DownloadFile(UPLOAD_URL, DOWNLOAD_FILE_NAME);
+            StartCoroutine(m_downloadCoroutine);
+        });
+
+        // 停止下载
+        stopDownloadBtn.onClick.AddListener(() => 
+        {
+            StopDownload();
+        });
+    }
+
+    private void StopDownload()
+    {
+        if (null != m_downloadCoroutine)
+        {
+            StopCoroutine(m_downloadCoroutine);
+            m_downloadCoroutine = null;
+        }
+        if(null != m_serverResponseStream)
+        {
+            m_serverResponseStream.Close();
+            m_serverResponseStream.Dispose();
+            m_serverResponseStream = null;
+
+            m_localFileStream.Close();
+            m_localFileStream.Dispose();
+            m_localFileStream = null;
+        }
+    }
+
+    /// <summary>
+    /// 读取文件的字节流
+    /// </summary>
+    /// <returns></returns>
+    private byte[] ReadLogFile(string filePath)
+    {
+        byte[] data = null;
+
+        using (FileStream fs = File.OpenRead(filePath))
+        {
+            int index = 0;
+            long len = fs.Length;
+            data = new byte[len];
+            int offset = data.Length > 1024 ? 1024 : data.Length;
+            while (index < len)
+            {
+                int readByteCnt = fs.Read(data, index, offset);
+                index += readByteCnt;
+                long leftByteCnt = len - index;
+                offset = leftByteCnt > offset ? offset : (int)leftByteCnt;
+            }
+            Debug.Log("Read Done");
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 通过http post上传文件
+    /// </summary>
+    /// <param name="url">http地址</param>
+    /// <param name="data">要上传的文件的字节流</param>
+    /// <returns></returns>
+    IEnumerator UploadFile(string url, string fileName, byte[] data)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("desc", "test upload file");
+        form.AddBinaryData("value", data, fileName, "application/x-gzip");
+        // 使用UnityWebRequest
+        UnityWebRequest request = UnityWebRequest.Post(url, form);
+        var result = request.SendWebRequest();
+        if (request.isNetworkError)
+        {
+            Debug.LogError(request.error);
+        }
+        while (!result.isDone)
+        {
+            yield return null;
+            // 更新上传日志进度条
+            progressSlider.value = request.uploadProgress;
+            progressLbl.text = Math.Floor(request.uploadProgress * 100) + "%";
+        }
+
+        Debug.Log("finish upload, http return msg: \n" + request.downloadHandler.text);
+    }
+
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="url">http地址</param>
+    /// <param name="fileName">要下载的文件名</param>
+    /// <returns></returns>
+    IEnumerator DownloadFile(string url, string fileName)
+    {
+        Debug.Log("DownloadFile");
+        //Head方法可以获取到文件的全部长度
+        UnityWebRequest huwr = UnityWebRequest.Head(url + "/" + fileName);
+        yield return huwr.SendWebRequest();
+        if (huwr.isNetworkError || huwr.isHttpError)
+        {
+            //如果出错, 输出 错误信息
+            Debug.Log(huwr.error);
+            yield break;
+        }
+        
+        //首先拿到文件的全部长度
+        long totalLength = long.Parse(huwr.GetResponseHeader("Content-Length"));
+        Debug.Log("totalLength: " + totalLength);
+#if UNITY_EDITOR
+        var filePath = Application.streamingAssetsPath + "/" + fileName;
+#else
+        // 真机上保存到persistentDataPath路径中
+        var filePath = Application.persistentDataPath + "/" + fileName;
+#endif
+        using (m_localFileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+        {
+            long nowFileLength = m_localFileStream.Length; //当前文件长度
+            Debug.Log(m_localFileStream.Length);
+            if (nowFileLength < totalLength)
+            {
+                //从头开始索引，长度为当前文件长度
+                m_localFileStream.Seek(nowFileLength, SeekOrigin.Begin);
+                Debug.Log("seek : " + nowFileLength);
+            }
+            else
+            {
+                Debug.Log("dont need download");
+                yield break;
+            }
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url + "/" + fileName);
+            request.AddRange(nowFileLength);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            m_serverResponseStream = response.GetResponseStream();
+            Debug.Log("responseStream.Length: " + m_serverResponseStream.Length);
+            int readSize = 0;
+            while (true)
+            {
+                readSize = m_serverResponseStream.Read(m_downloadBuffer, 0, m_downloadBuffer.Length);
+                if (readSize > 0)
+                {
+                    // 写入文件 
+                    m_localFileStream.Write(m_downloadBuffer, 0, readSize);
+                    nowFileLength += readSize;
+                    //展示下载进度
+                    progressSlider.value = (float)nowFileLength / totalLength;
+                    progressLbl.text = Math.Floor((float)nowFileLength / totalLength * 100) + "%";
+                }
+                else
+                {
+                    // 下载完成了
+                    progressSlider.value = 1;
+                    progressLbl.text = 100 + "%";
+                    m_serverResponseStream.Close();
+                    m_serverResponseStream.Dispose();
+                    break;
+                }
+                yield return null;
+            }
+            Debug.Log("download Done");
+
+        }
+    }
+}
---
+++ PythonServer/webserver.py
@@ -0,0 +1,130 @@
+import cgi
+import mimetypes
+import posixpath
+import shutil
+import urllib.parse
+import os
+import re
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+host = '127.0.0.1' 
+port = 8988 
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    if not mimetypes.inited:
+        # try to read system mime.types
+        mimetypes.init()  
+    extensions_map = mimetypes.types_map.copy()
+    extensions_map.update({
+        '': 'application/octet-stream',  # Default
+        '.py': 'text/plain',
+        '.c': 'text/plain',
+        '.h': 'text/plain',
+    })
+    
+    def do_GET(self):
+        f = self.send_head()
+        if f:
+            self.copyfile(f, self.wfile)
+            f.close()
+            
+    def do_HEAD(self):
+        f = self.send_head()
+        if f:
+            f.close()
+            
+    def send_head(self):
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+
+        ctype = self.guess_type(path)
+        offset = 0
+        try:
+            f = open(path, 'rb')
+            # 支持断点续传
+            content_ranges = self.headers['Range']
+            if None != content_ranges:
+                # Range: bytes=n-m
+                match = re.match('bytes=(\d+)-(\d*)', content_ranges)
+                if match:
+                    offset = int(match.group(1))
+                    f.seek(offset)
+                    print('f.seek ' + str(offset))
+        except IOError:
+            self.send_error(404, "File not found")
+            return None
+        self.send_response(200)
+        self.send_header("Content-type", ctype)
+        # 返回文件描述符fd的状态
+        fs = os.fstat(f.fileno())        
+        realsize = fs.st_size
+        self.send_header("Content-Length", str(realsize - offset))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+        self.end_headers()
+        return f
+
+    def guess_type(self, path):
+        base, ext = posixpath.splitext(path)
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
+        ext = ext.lower()
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
+        else:
+            return self.extensions_map['']
+
+    def translate_path(self, path):
+        path = path.split('?', 1)[0]
+        path = path.split('#', 1)[0]
+        path = posixpath.normpath(urllib.parse.unquote(path))
+        words = path.split('/')
+        words = [_f for _f in words if _f]
+        path = os.getcwd()
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir):
+                continue
+            path = os.path.join(path, word)
+        return path
+    
+    def copyfile(self, source, outputfile):
+        shutil.copyfileobj(source, outputfile)
+    
+    def do_POST(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     }
+        )
+        self.send_response(200)
+        self.end_headers()
+        desc = form['desc'].value
+        filename = form['value'].filename
+        filevalue = form['value'].value
+        filesize = len(filevalue)
+        with open(filename, 'wb') as f:
+                f.write(filevalue)
+        msg = 'upload success, file: %s, size:  %d, desc: %s'%(filename, filesize, desc)
+        print(msg)
+        self.wfile.write(msg.encode("utf-8"))
+        return
+
+ 
+if '__main__' == __name__ :
+    sever = HTTPServer((host, port), SimpleHTTPRequestHandler)
+    print("我是web服务器，地址：http://localhost:" + str(port))
+    sever.serve_forever()
